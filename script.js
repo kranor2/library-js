b10016835003509fbb2f1fc00020024201e5920f
Rhett North <retsrawk@gmail.com>
2024-02-02 21:02:40 -0700

    Add form + display function
    In this commit, I've added a form to the HTML in the sidebar, so as to
    create a new book. From there, I added the addBook() function, which
    sets each variable for info about the book to the associated form input
    values.

--- script.js
+++ script.js
@@ -18,6 +18,26 @@ function addBookToLibrary() {
     console.log(myLibrary.indexOf(book));
 }
 
+// prevent default - enable use of dialog form method
+
+document.getElementById("add-book").addEventListener("submit", function(event) {
+    event.preventDefault();
+    addBook(event);
+})
+
+// step 4 - add form and connect values to variables within object constructor
+
+function addBook() {
+    console.log("addBook function called");
+    const title = document.getElementById("book-title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").checked;
+    const index = myLibrary.length;
+
+    addBookToLibrary();
+}
+
 // step 3 - loop through array and display each book on the page
 
 function displayBook() {
@@ -27,6 +47,14 @@ function displayBook() {
         const listing = document.createElement("div");
         listing.className = "listing";
         const book = myLibrary[i];
+        listing.innerHTML = `
+        <div class-"listing-start">
+        <h2>${book.title}</h2>
+        <p>Author: ${book.author}</p>
+        <p>Pages: ${book.pages}</p></div>
+        <div class="listing-end"><p>Read: ${book.read ? `<input type="checkbox" id="read" checked>` : `<input type="checkbox" id="unread">`}</p>
+        <button type="button" class="rmv-btn" data-index="${i}><i class="material-icons">delete</i></button></div>`
+        ;
         shelf.insertBefore(listing, shelf.firstChild);
     }
 }
