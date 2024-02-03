61c6d9340cd30502d45dab3f2df3aa14a7245b2b
Rhett North <retsrawk@gmail.com>
2024-02-02 20:50:24 -0700

    Add displayBook()
    In this commit, I've added a function that will loop through the array
    and, given more code that I will add alongside a form in a future
    commit, display each book with its accompanying info on the page.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let entry;
 
 const myLibrary = [];
 
+// step 2 - add object constructor, add each object to myLibrary
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -14,4 +16,17 @@ function addBookToLibrary() {
     myLibrary.push(book);
     console.log(myLibrary);
     console.log(myLibrary.indexOf(book));
+}
+
+// step 3 - loop through array and display each book on the page
+
+function displayBook() {
+    const shelf = document.getElementById("shelf");
+    shelf.innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+        const listing = document.createElement("div");
+        listing.className = "listing";
+        const book = myLibrary[i];
+        shelf.insertBefore(listing, shelf.firstChild);
+    }
 }
